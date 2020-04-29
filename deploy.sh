6e47c570aa6b3772543bf6fe77e183229cad12bb
AbelJ <abelmendivil@gmail.com>
2020-04-29 17:00:20 +1200

    publishing tagged images

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,17 @@
-docker build -t abeljm/udemy-multi-client -f ./client/Dockerfile ./client
-docker build -t abeljm/udemy-multi-server -f ./server/Dockerfile ./server
-docker build -t abeljm/udemy-multi-worker -f ./worker/Dockerfile ./worker
-docker push abeljm/udemy-multi-client
-docker push abeljm/udemy-multi-server
-docker push abeljm/udemy-multi-worker
+docker build -t abeljm/udemy-multi-client:latest -t abeljm/udemy-multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t abeljm/udemy-multi-server:latest -t abeljm/udemy-multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t abeljm/udemy-multi-worker:latest -t abeljm/udemy-multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push abeljm/udemy-multi-client:latest
+docker push abeljm/udemy-multi-client:$SHA
+
+docker push abeljm/udemy-multi-server:latest
+docker push abeljm/udemy-multi-server:$SHA
+
+docker push abeljm/udemy-multi-worker:latest
+docker push abeljm/udemy-multi-worker:$SHA
+
 kubectl apply -f k8s
-#kubectl set image deployments/server-deployment server=abeljm/udemy-multi-server
+kubectl set image deployments/server-deployment server=abeljm/udemy-multi-server:$SHA
+kubectl set image deployments/client-deployment client=abeljm/udemy-multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=abeljm/udemy-multi-worker:$SHA
